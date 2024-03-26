61170a2131cbb2184a2ed2688c25b63b6a1d9a0a
leejnull <leejnull@gmail.com>
2024-03-26 15:09:39 +0800

    :sparkles: 邻接表

---
+++ graph/graph_adjacency_list.c
@@ -0,0 +1,136 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#define MAX_SIZE 64
+
+typedef struct {
+    
+} Vertex;
+
+typedef struct AdjListNode {
+    Vertex *vertex;
+    struct AdjListNode *next;
+} AdjListNode;
+
+typedef struct {
+    AdjListNode *heads[MAX_SIZE];
+    int size;
+} GraphAdjList;
+
+GraphAdjList *newGraphAdjList() {
+    GraphAdjList *graph = malloc(sizeof(GraphAdjList));
+    if (!graph) {
+        return NULL;
+    }
+    graph->size = 0;
+    for (int i = 0; i < MAX_SIZE; i++) {
+        graph->heads[i] = NULL;
+    }
+    return graph;
+}
+
+void delGraphAdjList(GraphAdjList *graph) {
+    for (int i = 0; i < graph->size; i++) {
+        AdjListNode *cur = graph->heads;
+        while (cur != NULL) {
+            AdjListNode *next = cur->next;
+            if (cur != graph->heads[i]) {
+                free(cur);
+            }
+            cur = next;
+        }
+        free(graph->heads[i]->vertex);
+        free(graph->heads[i]);
+    }
+    free(graph);
+}
+
+AdjListNode *findNode(GraphAdjList *graph, Vertex *vet) {
+    for (int i = 0; i < graph->size; i++) {
+        if (graph->heads[i]->vertex == vet) {
+            return graph->heads[i];
+        }
+    }
+    return NULL;
+}
+
+void addEdgeHelper(AdjListNode *head, Vertex *vet) {
+    AdjListNode *node = malloc(sizeof(AdjListNode));
+    node->vertex = vet;
+    node->next = head->next;
+    head->next = node;
+}
+
+void addEdge(GraphAdjList *graph, Vertex *vet1, Vertex *vet2) {
+    AdjListNode *head1 = findNode(graph, vet1);
+    AdjListNode *head2 = findNode(graph, vet2);
+    assert(head1 != NULL && head2 != NULL && head1 != head2);
+    addEdgeHelper(head1, vet2);
+    addEdgeHelper(head2, vet1);
+}
+
+void removeEdgeHelper(AdjListNode *head, Vertex *vet) {
+    AdjListNode *pre = head;
+    AdjListNode *cur = head->next;
+    while (cur != NULL && cur->vertex != vet) {
+        pre = cur;
+        cur = cur->next;
+    }
+    if (cur == NULL) {
+        return;
+    }
+    pre->next = cur->next;
+    free(cur);
+}
+
+void removeEdge(GraphAdjList *graph, Vertex *vet1, Vertex *vet2) {
+    AdjListNode *head1 = findNode(graph, vet1);
+    AdjListNode *head2 = findNode(graph, vet2);
+    assert(head1 != NULL && head2 != NULL && head1 != head2);
+    removeEdgeHelper(head1, vet2);
+    removeEdgeHelper(head2, vet1);
+}
+
+void addVertex(GraphAdjList *graph, Vertex *vet) {
+    assert(graph != NULL && graph->size < MAX_SIZE);
+    AdjListNode *node = malloc(sizeof(AdjListNode));
+    node->vertex = vet;
+    node->next = NULL;
+    graph->heads[graph->size++] = node;
+}
+
+void removeVertex(GraphAdjList *graph, Vertex *vet) {
+    AdjListNode *node = findNode(graph, vet);
+    assert(node != NULL);
+    AdjListNode *cur = node, *pre = NULL;
+    while (cur) {
+        pre = cur;
+        cur = cur->next;
+        free(pre);
+    }
+    for (int i = 0; i < graph->size; i++) {
+        cur = graph->heads[i];
+        pre = NULL;
+        while (cur) {
+            pre = cur;
+            cur = cur->next;
+            if (cur != NULL && cur->vertex == vet) {
+                pre->next = cur->next;
+                free(cur);
+                break;
+            }
+        }
+    }
+    int i;
+    for (i = 0; i < graph->size; i++) {
+        if (graph->heads[i] == node) {
+            break;
+        }
+    }
+    for (int j = i; j < graph->size - 1; j++) {
+        graph->heads[j] = graph->heads[j + 1];
+    }
+    graph->size--;
+    free(vet);
+}
