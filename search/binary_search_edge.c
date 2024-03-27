81532c176b96f3d8bec2ff768a93c5b397bd2710
leejnull <leejnull@gmail.com>
2024-03-27 22:42:03 +0800

    ✨ 二分查找
    查找右边界

--- search/binary_search_edge.c
+++ search/binary_search_edge.c
@@ -7,3 +7,15 @@ int binarySearchLeftEdge(int *nums, int numSize, int target) {
     }
     return i;
 }
+
+/*
+查找右边界等价于查找target+1的左边界
+*/
+int binarySearchRightEdge(int *nums, int numSize, int target) {
+    int i = binarySearchInsertion(nums, numSize, target + 1);
+    int j = i - 1;
+    if (j == -1 || nums[j] != target) {
+        return -1;
+    }
+    return j;
+}
