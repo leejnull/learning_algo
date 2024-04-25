af3bc58d1b61f4f52333371fa19d4d404d65478c
leejnull <leejnull@gmail.com>
2024-04-25 10:02:49 +0800

    :sparkles: 动态规划
    0-1背包问题暴力搜索

---
+++ question/knapsack.c
@@ -0,0 +1,24 @@
+/**
+ * 给定 n 个物品，第 i 个物品的重量为 wgt[i-1]、价值为 val[i-1]，和一个容量为 cap 的背包。
+ * 每个物品只能选择一次，问在限定背包容量下能放入物品的最大价值。
+*/
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+/* 0-1 背包：暴力搜索 */
+int knapspackDFS(int wgt[], int val[], int i, int c) {
+    // 若已选完所有物品或背包无剩余容量，则返回价值0
+    if (i == 0 || c == 0) {
+        return 0;
+    }
+    // 若超过背包容量，则只能选择跳过
+    if (wgt[i - 1] > c) {
+        return knapspackDFS(wgt, val, i - 1, c);
+    }
+    // 计算不放入和放入物品 i 的最大价值
+    int no = knapspackDFS(wgt, val, i - 1, c);
+    int yes = knapspackDFS(wgt, val, i - 1, c - wgt[i - 1] + val[i - 1]);
+    return max(no, yes);
+}
